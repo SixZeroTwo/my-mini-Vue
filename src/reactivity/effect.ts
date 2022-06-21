a233f6b891a7e30c2900568afc0c71462e8f204d
SixZeroTwo <454690182@qq.com>
2022-06-21 17:46:21 +0800

    effect的stop和onStop功能完成

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,30 +1,60 @@
+import { extend } from "./shared/extend"
+
 class ReactiveEffect {
   private fn: Function
+  public scheduler?
+  public onStop?: Function
+  public deps: Set<any> = new Set
+  active: boolean = true
   constructor(fn) {
     this.fn = fn
   }
   run() {
     activeEffect = this
     this.fn()
+    this.active = true
     return this.fn
   }
+  stop() {
+    if (this.active) {
+      cleanEffect(this)
+      //执行onStop回调
+      this.onStop && this.onStop()
+      this.active = false
+    }
+  }
 }
+function cleanEffect(effect) {
 
-export function effect(fn: Function) {
+  //找到deps并一一删除
+  effect.deps.forEach(dep => dep.delete(effect))
+  //清空活动对象
+  activeEffect = null
+}
+export function effect(fn: Function, options?: any) {
   //构造一个ReactiveEffect实例
   let _effect = new ReactiveEffect(fn)
-  return _effect.run().bind(_effect)
+  //将options的属性赋予_effect
+  extend(_effect, options)
+  const runner = _effect.run().bind(_effect)
+  runner._effect = _effect
+  return runner
 }
-let activeEffect: ReactiveEffect
+let activeEffect: ReactiveEffect | null
 const targetsMap = new Map
+
 export function track(target, key) {
   //读取targetsMap中target对应的depsMap
   if (!targetsMap.has(target)) targetsMap.set(target, new Map())
   let depsMap = targetsMap.get(target)
   if (!depsMap.has(key)) depsMap.set(key, new Set)
-  let deps = depsMap.get(key)
+  let dep = depsMap.get(key)
+  //当前如果没有活动对象
+  if (!activeEffect) return
   //将现在的effect对象放入deps
-  deps.add(activeEffect)
+  dep.add(activeEffect)
+  //反向记录，每个Effect对象都会记录自己所在的deps
+  activeEffect.deps.add(dep)
 }
 
 export function trigger(target, key, value) {
@@ -32,9 +62,14 @@ export function trigger(target, key, value) {
   if (!targetsMap.has(target)) targetsMap.set(target, new Map())
   let depsMap = targetsMap.get(target)
   if (!depsMap.has(key)) depsMap.set(key, new Set)
-  let deps = depsMap.get(key)
+  let dep = depsMap.get(key)
   //通知依赖
-  for (let dep of deps) {
-    dep.run()
+  for (let effect of dep) {
+    if (effect.scheduler) effect.scheduler()
+    else effect.run()
   }
+}
+
+export function stop(runner) {
+  runner._effect.stop()
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from '../effect'
+import { effect, stop } from '../effect'
 import { reactive } from "../reactive"
 describe('effect', () => {
   it('happy path', () => {
@@ -26,5 +26,45 @@ describe('effect', () => {
     const n = fn()
     expect(foo).toBe(3)
     expect(n).toBe('foo')
+  });
+  //测试scheduler功能
+  it('scheduler ', () => {
+    let run: any
+    let scheduler = function () {
+      run = runner
+    }
+    let foo = reactive({ bar: 1 })
+    let bee
+    let runner = effect(() => {
+      bee = foo.bar + 1
+      return 'runner'
+    }, {
+      scheduler: scheduler
+    })
+    expect(bee).toBe(2)
+    //trigger，执行的不是fn，而是scheduler
+    foo.bar++//foo.bar == 2 ，没有执行fn，所以bee还是2 ，执行了scheduler，所以run可以使用
+    expect(bee).toBe(2)
+    const n = run()//执行run，即执行fn函数，此时bee == foo.bar +1 ==3，且n为fn的返回值
+    expect(bee).toBe(3)
+    expect(n).toBe('runner')
+  });
+  //测试stop和onStop
+  it('stop and onStop', () => {
+    let foo = reactive({ bar: 1 })
+    let dummy
+    let str = ''
+    let onStop = jest.fn(() => { str += 'stop!' })
+    let runner = effect(() => {
+      dummy = foo.bar
+    }, { onStop })
+    expect(dummy).toBe(1)
+    stop(runner)
+    foo.bar++
+    //foo.bar++ 如果是这种写法，测试不通过
+    expect(dummy).toBe(1)//dummy失去了响应性
+    expect(onStop).toBeCalledTimes(1)//onStop被调用
+    runner()
+    expect(dummy).toBe(2)//dummy恢复响应性
   });
 })
