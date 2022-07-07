d8038814f4b3293f755b06dec929d3788048c2ed
SixZeroTwo <454690182@qq.com>
2022-07-07 13:46:20 +0800

    修改了作用域插槽的写法

--- example/componentSlots/App.js
+++ example/componentSlots/App.js
@@ -8,7 +8,7 @@ export const App = {
   },
   render() {
     const app = h('div', {}, 'App')
-    const foo = h(Foo, {}, { header: ({ msg }) => h('p', {}, '插槽1' + msg), footer: [() => h('p', {}, '插槽2'), () => h('p', {}, '插槽3')] })
+    const foo = h(Foo, {}, { header: ({ msg }) => h('p', {}, '插槽1' + msg), footer: () => [h('p', {}, '插槽2'), h('p', {}, '插槽3')] })
     // ui
     return h('div', {}, [app, foo])
   },
--- src/runtime-core/src/componentSlots.ts
+++ src/runtime-core/src/componentSlots.ts
@@ -8,10 +8,7 @@ export function initSlots(instance, vnode) {
 export function renderSlots(slots, name, slotsProps?) {
   //判断一下有没有插槽内容
   const slot = slots[name]
-  if (Array.isArray(slot)) {
-    return h('div', {}, slot.map(item => handleSlot(item, slotsProps)))
-  }
-  else if (typeof slot == 'function') return handleSlot(slot, slotsProps)
+  if (slot) return h('div', {}, handleSlot(slot, slotsProps))
 }
 
 function handleSlot(slot, slotsProps) {
